dc27c80051848545db1444b877252db1c3bfd9ee
wdurno <wdurno@kpmg.com>
2020-02-19 07:34:40 -0800

    standalone spark installed on image

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 export RL_HYPOTHESIS_1_DOCKER_REGISTRY_HEAD=gcr.io/gdax-dnn/
-export RL_HYPOTHESIS_1_DOCKER_IMAGE=gcr.io/gdax-dnn/rl-hypothesis-1-env@sha256:a195eefae832837def538eaf5ccec47dd913c7d6253272975ffe26601336ef8a
+export RL_HYPOTHESIS_1_DOCKER_IMAGE=gcr.io/gdax-dnn/rl-hypothesis-1-env@sha256:a05150e08e3d6d21046ff05404fb0d649a9b877c3bd0311040549f35b36ae7f3
 export RL_HYPOTHESIS_1_SERVICE_ACCOUNT_JSON_PATH=/home/wdurno/project-service-account.json
 export RL_HYPOTHESIS_1_BUCKET=gdax-dnn-tmp-1
 export RL_HYPOTHESIS_1_PROJECT=gdax-dnn
---
+++ scripts/spin-up-cluster.sh
@@ -0,0 +1,22 @@
+gcloud beta container --project "gdax-dnn" clusters create "standard-cluster-1" \
+	--zone "us-central1-a" \
+	--no-enable-basic-auth \
+	--cluster-version "1.14.10-gke.17" \
+	--machine-type "n1-standard-2" \
+	--image-type "COS" \
+	--disk-type "pd-standard" \
+	--disk-size "100" \
+	--metadata disable-legacy-endpoints=true \
+	--scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+	--num-nodes "3" \
+	--enable-stackdriver-kubernetes \
+	--enable-ip-alias \
+	--network "projects/gdax-dnn/global/networks/default" \
+	--subnetwork "projects/gdax-dnn/regions/us-central1/subnetworks/default" \
+	--default-max-pods-per-node "110" \
+	--enable-autoscaling \
+	--min-nodes "3" \
+	--max-nodes "10" \
+	--addons HorizontalPodAutoscaling,HttpLoadBalancing \
+	--enable-autoupgrade \
+	--enable-autorepair
