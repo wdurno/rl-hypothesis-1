a6e984155b547db9e8922ed0181e984f50e88126
wdurno <wdurno@kpmg.com>
2020-02-20 08:22:34 -0800

    k8s + spark functional

--- scripts/spin-up-cluster.sh
+++ scripts/spin-up-cluster.sh
@@ -8,7 +8,7 @@ gcloud beta container --project "gdax-dnn" clusters create "${RL_HYPOTHESIS_1_IN
 	--disk-size "100" \
 	--metadata disable-legacy-endpoints=true \
 	--scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-	--num-nodes "6" \
+	--num-nodes "10" \
 	--enable-stackdriver-kubernetes \
 	--enable-ip-alias \
 	--network "projects/gdax-dnn/global/networks/default" \
