4f267d73ca7e241f03798d673e0ac61c0c2b8973
wdurno <wdurno@kpmg.com>
2020-03-15 18:16:39 -0700

    cvae operational; needs tuning; code messy

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 export RL_HYPOTHESIS_1_DOCKER_REGISTRY_HEAD=gcr.io/gdax-dnn/
-export RL_HYPOTHESIS_1_DOCKER_IMAGE=gcr.io/gdax-dnn/rl-hypothesis-1-env@sha256:4f104a10bf8639fb509671457ac940db250cbf86676457608055d534f3f6b90e
+export RL_HYPOTHESIS_1_DOCKER_IMAGE=gcr.io/gdax-dnn/rl-hypothesis-1-env@sha256:1848aa13f9f2097efcf8222b6b2c80bf01ec4c93812e5e79e00b49243389f7c0
 export RL_HYPOTHESIS_1_SERVICE_ACCOUNT_JSON_PATH=/home/wdurno/project-service-account.json
 export RL_HYPOTHESIS_1_BUCKET=gdax-dnn-tmp-1
 export RL_HYPOTHESIS_1_PROJECT=gdax-dnn
