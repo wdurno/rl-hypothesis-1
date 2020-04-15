2d0fd7cd82c22838e2e050f6a85f467aa0ea1690
wdurno <wdurno@kpmg.com>
2020-04-14 20:37:58 -0700

    cvae needs a lot of tuning

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 export RL_HYPOTHESIS_1_DOCKER_REGISTRY_HEAD=gcr.io/gdax-dnn/
-export RL_HYPOTHESIS_1_DOCKER_IMAGE=gcr.io/gdax-dnn/rl-hypothesis-1-env@sha256:3fe1d4eb131b292d796f685ffde54ebe72256f12506017259c90fd0d70046070
+export RL_HYPOTHESIS_1_DOCKER_IMAGE=gcr.io/gdax-dnn/rl-hypothesis-1-env@sha256:fef1ab97b97692098c9274d4be92065415844a448eeb7dd5b26b4891d163bfd6
 export RL_HYPOTHESIS_1_SERVICE_ACCOUNT_JSON_PATH=/home/wdurno/project-service-account.json
 export RL_HYPOTHESIS_1_BUCKET=gdax-dnn-tmp-1
 export RL_HYPOTHESIS_1_PROJECT=gdax-dnn
